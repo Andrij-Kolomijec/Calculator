bfffeec3ff4b3fe541b2983c7c0576072b79542a
Difficins <a.kolomijec@seznam.cz>
2023-07-06 17:04:24 +0200

    Displaying and calculatig results in the first row

--- javascript.js
+++ javascript.js
@@ -14,9 +14,9 @@ function divide(a, b) {
     return a / b
 }
 
-let a;
-let operator;
-let b;
+let firstRow = null;
+let operator = null;
+let secondRow = null;
 
 function operate(a, operator, b) {
     if (operator === "+") {
@@ -33,5 +33,25 @@ function operate(a, operator, b) {
 const displayFirstRow = document.querySelector('#display-first-row');
 const displaySecondRow = document.querySelector('#display-second-row');
 const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => button.addEventListener('click', (e) => displaySecondRow.textContent = e.target.textContent))
+buttons.forEach((button) => button.addEventListener('click', (e) => displaySecondRow.textContent = displaySecondRow.textContent + e.target.textContent))
  
+const actions = document.querySelectorAll('.last-button');
+actions.forEach((action) => action.addEventListener('click', (e) => {
+    
+    displayFirstRow.textContent = displayFirstRow.textContent + displaySecondRow.textContent;
+    displaySecondRow.textContent = '';
+    }))
+
+const equals = document.querySelector('#equal-button');
+equals.addEventListener('click', )
+
+
+
+
+
+
+
+// zobrazit cele cislo
+// po stisknuti jineho tlacitka (krome desetinne carky) 
+//      nez cislo zobrazit toto cislo a znamenko v radku nad
+// 
