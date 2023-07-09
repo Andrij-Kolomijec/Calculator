ff13ac9e10bdc3dead4cdfd689eca5a163e7015c
Difficins <a.kolomijec@seznam.cz>
2023-07-09 13:31:50 +0200

    Equal button works correctly

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
 function add(a, b) {
-    return a + b
+    return Number(a) + Number(b)
 }
 
 function subtract(a, b) {
-    return a - b
+    return Number(a) - Number(b)
 }
 
 function multiply(a, b) {
-    return a * b
+    return Number(a) * Number(b)
 }
 
 function divide(a, b) {
-    return a / b
+    return Number(a) / Number(b)
 }
 
 let firstRow = null;
@@ -23,27 +23,49 @@ function operate(a, operator, b) {
         return add(a, b)
     } else if (operator === "-") {
         return subtract(a, b)
-    } else if (operator === "*") {
+    } else if (operator === "×") {
         return multiply(a, b)
-    } else if (operator === "/") {
+    } else if (operator === "÷") {
         return divide(a, b)
     }
 }
 
 const displayFirstRow = document.querySelector('#display-first-row');
 const displaySecondRow = document.querySelector('#display-second-row');
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => button.addEventListener('click', (e) => displaySecondRow.textContent = displaySecondRow.textContent + e.target.textContent))
+const buttons = document.querySelectorAll('.numeric');
+buttons.forEach((button) => button.addEventListener('click', (e) => {
+    if (finished === true) {
+        displaySecondRow.textContent = '';
+        displayFirstRow.textContent = '';
+        finished = false;
+    }
+    displaySecondRow.textContent = displaySecondRow.textContent + e.target.textContent;
+    secondRow = displaySecondRow.textContent;
+    }
+))
  
 const actions = document.querySelectorAll('.last-button');
 actions.forEach((action) => action.addEventListener('click', (e) => {
-    // if both first row and second row are not null calculate and the result operator another number 
-    displayFirstRow.textContent = displayFirstRow.textContent + displaySecondRow.textContent;
+    if (firstRow !== null) {
+            displayFirstRow.textContent = operate(firstRow, operator, secondRow);
+            operator = e.target.textContent;
+        } else {
+            firstRow = secondRow;
+            operator = e.target.textContent;
+        }
+    displayFirstRow.textContent = firstRow + operator;
     displaySecondRow.textContent = '';
     }))
 
+let finished = false;
+
 const equals = document.querySelector('#equal-button');
-equals.addEventListener('click', )
+equals.addEventListener('click', () => {
+    displayFirstRow.textContent = firstRow + operator + secondRow;
+    displaySecondRow.textContent = operate(firstRow, operator, secondRow);
+    finished = true;
+    }
+);
 
 
 
